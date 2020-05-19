4164a58a9a7185a266dc71375b04c1355caf8905
cubetimeATL <65615167+cubetimeATL@users.noreply.github.com>
2020-05-19 12:31:17 -0400

    Add files via upload

---
+++ js/egg.js
@@ -0,0 +1,6 @@
+var egg = new Egg("up,up,down", function() {
+    jQuery('#egggif').fadeIn(500, function() {
+      window.setTimeout(function() { jQuery('#egggif').hide(); }, 5000);
+    });
+    console.log("ur mom gay");
+  }).listen();
